c678414c8bb188508349c9511c65f2be12e32ea9
jabreu66 <120123188+jabreu66@users.noreply.github.com>
2026-02-16 16:59:45 -0500

    working on gating for sensors

--- src/est_state.cpp
+++ src/est_state.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "est_state.h"
+#include <cmath>
+
 
 estimated_state::estimated_state(double pos, double vel, double acc, double p_var, double v_var, double a_var) : x(pos), vx(vel), ax(acc) 
 {
@@ -111,50 +113,62 @@ estimated_state estimated_state::correction(double x_pred, double vx_pred, doubl
 if(has_gps){
    double gps_innovation = x_gps - x_pred;
 
-   total_uncertainty = this->cov_matrix[0][0] + R_gps;
-   
-   Kx = this->cov_matrix[0][0] / total_uncertainty;
-   Kv = this->cov_matrix[1][0] / total_uncertainty; 
-   Ka = this->cov_matrix[2][0] / total_uncertainty;
-
-   x_pred = x_pred + Kx * gps_innovation;
-   vx_pred = vx_pred + Kv * gps_innovation;
-   ax_pred = ax_pred + Ka * gps_innovation;
-
-   double row0[3] = {this->cov_matrix[0][0], this->cov_matrix[0][1], this->cov_matrix[0][2]};
-   double K[3] = {Kx, Kv, Ka};
-
-   for(int i = 0; i < 3; i++)
-   {
-        for(int j = 0; j < 3; j++)
-        {
-            this->cov_matrix[i][j] -= K[i] * row0[j]; 
-        }
+   if(std::abs(gps_innovation) > 50){
+        has_gps = false;
    }
-}
+
+   if(has_gps){
+        total_uncertainty = this->cov_matrix[0][0] + R_gps;
+        
+        Kx = this->cov_matrix[0][0] / total_uncertainty;
+        Kv = this->cov_matrix[1][0] / total_uncertainty; 
+        Ka = this->cov_matrix[2][0] / total_uncertainty;
+
+        x_pred = x_pred + Kx * gps_innovation;
+        vx_pred = vx_pred + Kv * gps_innovation;
+        ax_pred = ax_pred + Ka * gps_innovation;
+
+        double row0[3] = {this->cov_matrix[0][0], this->cov_matrix[0][1], this->cov_matrix[0][2]};
+        double K[3] = {Kx, Kv, Ka};
+
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                this->cov_matrix[i][j] -= K[i] * row0[j]; 
+            }
+        }
+    }
+}   
    // vel sec
 if(has_vel){
     double vel_innovation = vx_meas - vx_pred;
-    total_uncertainty = this->cov_matrix[1][1] + R_vel;
-   
-    Kx = this->cov_matrix[0][1] / total_uncertainty;
-    Kv = this->cov_matrix[1][1] / total_uncertainty;
-    Ka = this->cov_matrix[2][1] / total_uncertainty;
+    if(std::abs(vel_innovation) > 20)
+    {
+        has_vel = false;
+    }
+            if(has_vel){
+            total_uncertainty = this->cov_matrix[1][1] + R_vel;
+        
+            Kx = this->cov_matrix[0][1] / total_uncertainty;
+            Kv = this->cov_matrix[1][1] / total_uncertainty;
+            Ka = this->cov_matrix[2][1] / total_uncertainty;
 
-   x_pred = x_pred + Kx * vel_innovation;
-   vx_pred = vx_pred + Kv * vel_innovation;
-   ax_pred = ax_pred + Ka * vel_innovation;
+        x_pred = x_pred + Kx * vel_innovation;
+        vx_pred = vx_pred + Kv * vel_innovation;
+        ax_pred = ax_pred + Ka * vel_innovation;
 
-   double row1[3] = {this->cov_matrix[1][0], this->cov_matrix[1][1], this->cov_matrix[1][2]};
-   double vK[3] = {Kx, Kv, Ka};
+        double row1[3] = {this->cov_matrix[1][0], this->cov_matrix[1][1], this->cov_matrix[1][2]};
+        double vK[3] = {Kx, Kv, Ka};
 
-   for(int i = 0; i < 3; i++)
-   {
-        for(int j = 0; j < 3; j++)
+        for(int i = 0; i < 3; i++)
         {
-            this->cov_matrix[i][j] -= vK[i] * row1[j]; 
+                for(int j = 0; j < 3; j++)
+                {
+                    this->cov_matrix[i][j] -= vK[i] * row1[j]; 
+                }
         }
-   }
+    }
 }
 
    estimated_state corrected(x_pred, vx_pred, ax_pred, this->cov_matrix[0][0], this->cov_matrix[1][1], this->cov_matrix[2][2]);
